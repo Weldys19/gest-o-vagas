e66ce749133536d1c8942e17e265c5e226b6635c
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2025-12-30 11:31:59 -0300

    Criando repositório

--- src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package com.br.weldyscarmo.gestao_vagas.modules.candidate.controllers;
 
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.CandidateEntity;
+import com.br.weldyscarmo.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getName());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+        return this.candidateRepository.save(candidateEntity);
     }
 }
