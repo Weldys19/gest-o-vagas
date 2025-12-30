b2af7477c24b4b819eb6847823c887f5a799de37
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2025-12-30 11:01:46 -0300

    Criando a tabela canditato

--- src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,24 @@
 package com.br.weldyscarmo.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -23,4 +32,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
 }
