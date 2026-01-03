373e65b006edc1f242e93004ded96b99f259a20f
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2026-01-02 21:03:48 -0300

    Hash da senha do candidato

--- src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.br.weldyscarmo.gestao_vagas.exceptions.UserFoundException;
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.CandidateEntity;
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,17 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity){
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
