c07b0e0af33e6ef08ed6ae52c80f6c8863d9a206
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2026-01-07 14:17:53 -0300

    UseCase: ListAllJobsByFilter

--- src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,7 +2,9 @@ package com.br.weldyscarmo.gestao_vagas.modules.candidate.controllers;
 
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.CandidateEntity;
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.br.weldyscarmo.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import com.br.weldyscarmo.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import com.br.weldyscarmo.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,6 +24,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -43,5 +49,11 @@ public class CandidateController {
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter){
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
